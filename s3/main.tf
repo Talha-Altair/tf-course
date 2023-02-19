233760c5cb43066c55a08046bef2862d6fcc07a9
Talha_Altair <cooltalha27@gmail.com>
2023-02-20 02:21:58 +0530

    tf

---
+++ s3/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+
+resource "aws_s3_bucket" "altair" {
+  bucket = "talha-altair-27"
+  tags = {
+    "num" = var.num_var
+  }
+}
